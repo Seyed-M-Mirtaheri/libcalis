b2e55b3c730a5d91788f7d2f3017e49b0c0b79f0
Anseok Joo <anseok@gmail.com>
2014-08-27 18:32:37 +0800

    Extended to allow both destructive & non-destructive sort.  Halved cacheSize to allow both src and dst arrays to fit in the cache.

--- calis.py
+++ calis.py
@@ -1,36 +1,44 @@
 #!/usr/bin/python
 
-# Python prototype of Cache-Aware Local Iterative Sort(libcalis)
+# Python prototype of Cache-Aware Local Iterative Sort (libcalis)
 # Author: Anseok Joo (anseok at gmail dot com)
 # Public Domain
 
-## customisables #######################################################
-CACHE_SIZE = 4
+import math
 
-## functions ###########################################################
+## customisables #######################################################
+CACHE_SIZE = 8
+
+## procedures ##########################################################
 def copy(src, srcOffset, size, dst, dstOffset):
   'copy sublist of size SIZE from SRC at SRCOFFSET to DST at DSTOFFSET'
   for i in xrange(size): dst[dstOffset + i] = src[srcOffset + i]
 
-def sort(src, f, cacheSize, comp = cmp):
+def sort(src, dst, f, cacheSize, comp = cmp):
   '''sort list SRC with sorting algorithm F using comparison function
-COMP assuming CPU cache size CACHESIZE'''
-  size, dst = len(src), [0 for _ in src]
-  if size <= cacheSize: return f(src, 0, size, dst, comp)
+COMP assuming CPU cache size CACHESIZE; set DST to SRC for destructive
+sort'''
+  cacheSize /= 2  # assuming we work with two lists of same size
+  size = len(src)
+  src = f(src, 0, size, [0 for _ in src], comp, iterations = 1)
+  if size <= cacheSize:
+    ret = f(src, 0, size, dst, comp, 2)
+    if ret != dst: copy(ret, 0, size, dst, 0)
+    return
   # whether the sorted sublist ends up in SRC or DST depends on F and
-  # CACHESIZE, which is variable; so we refer to sorted list with RET
-  ret = f(src, 0, cacheSize, dst, comp)
+  # CACHESIZE, which are variable; so we refer to sorted list with RET
+  ret = f(src, 0, cacheSize, dst, comp, 2)
   last = size - size % cacheSize
   for i in xrange(cacheSize, last, cacheSize):
-    f(src, i, cacheSize, dst, comp)
+    f(src, i, cacheSize, dst, comp, 2)
   tmpSize = min(cacheSize, size - last)
   if tmpSize:
     # last sublist's size differs from CACHESIZE, so sorted list might
     # not end up in RET
-    tmp = f(src, last, tmpSize, dst, comp)
+    tmp = f(src, last, tmpSize, dst, comp, 2)
     if tmp != ret: copy(tmp, last, tmpSize, ret, last)
-  if ret != src: src, dst = dst, src
-  return f(src, 0, size, dst, comp, cacheSize)
+  ret = f(ret, 0, size, (dst if ret == src else src), comp, cacheSize)
+  if ret != dst: copy(ret, 0, size, dst, 0)
 
 ## example sorting algorithm--merge sort ###############################
 def merge(src, aOffset, bOffset, bSize, dst, comp):
@@ -45,11 +53,14 @@ and output to DST starting at AOFFSET'''
   if bSize:   copy(src, bOffset, bSize, dst, dstOffset)
   elif aSize: copy(src, aOffset, aSize, dst, dstOffset)
 
-def mergesort(src, offset, size, dst, comp, aSize = 1):
+def mergesort(src, offset, size, dst, comp, aSize = 1, iterations = 0):
   '''the merge sort algorithm; uses comparison function CMP; ASIZE is
-the smallest block size to consider'''
+the smallest block size to consider; performs ITERATIONS levels of merge
+operations, full when 0'''
+  if not iterations:
+    iterations = int(math.ceil(math.log(float(size) / aSize, 2)))
   end = offset + size
-  while aSize < size:
+  for i in xrange(iterations):
     aOffset, bOffset, aaSize = offset, offset + aSize, aSize * 2
     while bOffset < end:
       bSize = min(aSize, end - bOffset)
@@ -63,7 +74,8 @@ the smallest block size to consider'''
 def main():
   n = 21
   src = range(n); random.shuffle(src); dst = [0 for _ in src]
-  print sort(src, mergesort, CACHE_SIZE, cmp)
+  sort(src, dst, mergesort, CACHE_SIZE, cmp)
+  print dst
 
 if __name__ == '__main__': import random; main()
 
